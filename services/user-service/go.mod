0263f508dc9c2992b70c08a0db0591b85c722238
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-21 18:34:39 +0900

    auth setup

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/gorilla/mux v1.6.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/markbates/goth v1.79.0 // indirect
 	golang.org/x/oauth2 v0.17.0 // indirect
--- services/user-service/internal/auth/auth.go
+++ services/user-service/internal/auth/auth.go
@@ -1,13 +1,41 @@
 package auth
 
 import (
-    "github.com/markbates/goth"
-    "github.com/markbates/goth/providers/google"
-    
+	"log"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 )
 
-func init() {
-    goth.UseProviders(
-        google.New("your-client-id", "your-client-secret", "http://localhost:8080/auth/google/callback"),
-    )
+const (
+	key = "randomKey"
+	MaxAge = 86400 * 30
+	IsProd = false
+)
+
+func NewAuth(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	googleClientID := os.Getenv("GOOGLE_CLIENT_ID")
+	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(MaxAge)
+
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true
+	store.Options.Secure = IsProd
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(googleClientID, googleClientSecret, "http://localhost:3000/auth/google/callback"),
+	)
 }
