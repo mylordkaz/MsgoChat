d834b6564900623efc4a73773cda36e4084f9df5
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-22 00:18:04 +0900

    syntax correction ("*")

--- services/user-service/internal/routes/routes.go
+++ services/user-service/internal/routes/routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/handlers"
 )
 
-func RegisterRoutes(router mux.Router){
+func RegisterRoutes(router *mux.Router){
 
 	router.HandleFunc("/users", handlers.CreateUser).Methods("POST")
 	router.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
