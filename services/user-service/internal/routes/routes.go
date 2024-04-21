d6aa0b1721c57be9e0e6bc25cb0f928de276d5e4
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-22 00:13:25 +0900

    create first user routes

---
+++ services/user-service/internal/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mylordkaz/MsgoChat/services/user-service/internal/handlers"
+)
+
+func RegisterRoutes(router mux.Router){
+
+	router.HandleFunc("/users", handlers.CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
+
+}
