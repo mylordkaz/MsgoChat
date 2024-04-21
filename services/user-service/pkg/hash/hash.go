5c48cbd76c373b278a795f9becf3112add9639c0
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-21 23:54:52 +0900

    create hash function

--- services/user-service/pkg/hash/hash.go
+++ services/user-service/pkg/hash/hash.go
@@ -1,4 +1,15 @@
 package hash
 
+import "golang.org/x/crypto/bcrypt"
 
+//create a bcrypt hash of the pwd
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    return string(bytes), err
+}
 
+// compare the hashed pwd
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
