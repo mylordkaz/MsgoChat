c1a240cf1d14f6cf9186310a174c49ff31267a58
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-21 22:56:05 +0900

    fix: move out database code

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -3,23 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/mylordkaz/MsgoChat/services/user-service/internal/database"
 )
 
-
-
 func main () {
-
-	dbHost := os.Getenv("BD_HOST")
-	dbPort := os.Getenv("BD_PORT")
-	dbUser := os.Getenv("BD_USER")
-	dbPass := os.Getenv("BD_PASS")
-	dbName := os.Getenv("BD_NAME")
-	
-	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s name=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
-	database.InitDB(dataSourceName)
 
 	http.HandleFunc("/", handleUser)
 	fmt.Println("Starting server at port 8080")
