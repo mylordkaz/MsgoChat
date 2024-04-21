5b04ec32f70bd958c3b78a145933fc9acfa982d1
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-22 00:07:31 +0900

    create Get user function

--- services/user-service/internal/handlers/userHandlers.go
+++ services/user-service/internal/handlers/userHandlers.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/database"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
 )
@@ -22,6 +23,15 @@ func CreateUser(w http.ResponseWriter, r *http.Request){
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	
 
+
+}
+func GetUser(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id := vars["id"]
+	user := models.User{UserID: "1", Name: "kevin", Email: "kevinv@gmail.com"} // fake data
+
+	if id == "1"{
+		json.NewEncoder(w).Encode(user)
+	}
 }
