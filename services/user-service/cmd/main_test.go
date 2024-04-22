bf06d966a6fde2aeb00f5d7c5c3318e3deae9f4e
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-23 00:10:00 +0900

    test for User handler

--- services/user-service/cmd/main_test.go
+++ services/user-service/cmd/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -12,4 +14,27 @@ func TestHandlerUser(t *testing.T){
 	router := mux.NewRouter()
 	routes.RegisterRoutes(router)
 	router.HandleFunc("/", handleUser)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	// create a ResponseRecorder to record the response 
+	rr := httptest.NewRecorder()
+	handler := http.Handler(router)
+
+	// dispach request to handler
+	handler.ServeHTTP(rr, req)
+
+	//check status code is what expected
+	if status := rr.Code; status != http.StatusOK{
+		t.Errorf("handler returned wrong status code : got %v want %v", status, http.StatusOK)
+	}
+
+	// check response body
+	expected := "Hello, you've reached the user service\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body : got %v want %v", rr.Body.String(), expected)
+	}
 }
