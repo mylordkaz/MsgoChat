6934ab95068b363a8afb249193f126c3a74ca185
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-04-23 23:45:54 +0900

    feat: complete createUser func & add hash logic

--- services/user-service/internal/handlers/userHandlers.go
+++ services/user-service/internal/handlers/userHandlers.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/database"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
+	"github.com/mylordkaz/MsgoChat/services/user-service/pkg/hash"
 )
 
 
@@ -19,11 +20,26 @@ func init(){
 
 func CreateUser(w http.ResponseWriter, r *http.Request){
 	var user models.User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+    if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	hashedPassword, err := hash.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
 		return
 	}
+	user.Password = hashedPassword
 
+    // Insert user into the database
+    err = db.QueryRow("INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING id", user.NickName, user.Email, user.Password).Scan(&user.IDToken)
+    if err != nil {
+        http.Error(w, "Error creating user", http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(user)
 
 }
 func GetUser(w http.ResponseWriter, r *http.Request){
@@ -34,4 +50,10 @@ func GetUser(w http.ResponseWriter, r *http.Request){
 	if id == "1"{
 		json.NewEncoder(w).Encode(user)
 	}
+}
+func UpdateUser(w http.ResponseWriter, r *http.Request){
+
+}
+func DeleteUser(w http.ResponseWriter, r *http.Request){
+
 }
