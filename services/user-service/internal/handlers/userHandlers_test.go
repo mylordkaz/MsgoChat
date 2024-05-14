a49a387798e801ee33ada537bfe63233be0cb92f
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-05-14 23:55:32 +0900

    feat: Get user test

--- services/user-service/internal/handlers/userHandlers_test.go
+++ services/user-service/internal/handlers/userHandlers_test.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gorilla/mux"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
@@ -39,4 +38,43 @@ func TestCreateUser(t *testing.T){
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
+}
+func TestGetUser(t *testing.T) {
+    mockDB, mock, err := sqlmock.New()
+    if err != nil {
+        t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+    }
+    defer mockDB.Close()
+    db = mockDB 
+
+    router := mux.NewRouter()
+    router.HandleFunc("/users/{id}", GetUser).Methods("GET")
+
+    user := models.User{
+        NickName: "jeanToad",
+        Email:    "jeanToad@gmail.com",
+        IDToken:  "1",
+    }
+
+    mock.ExpectQuery(`SELECT email, nickname, id_token FROM users WHERE id = \$1`).
+        WithArgs(user.IDToken).
+        WillReturnRows(sqlmock.NewRows([]string{"email", "nickname", "id_token"}).
+            AddRow(user.Email, user.NickName, user.IDToken))
+
+    request, _ := http.NewRequest("GET", "/users/"+user.IDToken, nil)
+    response := httptest.NewRecorder()
+
+    router.ServeHTTP(response, request)
+
+    if err := mock.ExpectationsWereMet(); err != nil {
+        t.Errorf("there were unfulfilled expectations: %s", err)
+    }
+
+    assert.Equal(t, http.StatusOK, response.Code, "Expected HTTP status 200")
+    var returnedUser models.User
+    json.NewDecoder(response.Body).Decode(&returnedUser)
+
+    assert.Equal(t, user.Email, returnedUser.Email, "Email should match")
+    assert.Equal(t, user.NickName, returnedUser.NickName, "Nickname should match")
+    assert.Equal(t, user.IDToken, returnedUser.IDToken, "IDToken should match")
 }
