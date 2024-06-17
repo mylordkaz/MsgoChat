c60f204e93aab846cb86180e390994ed9647ee33
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-17 10:56:19 +0900

    first commit auth routes

--- services/user-service/internal/routes/routes.go
+++ services/user-service/internal/routes/routes.go
@@ -6,10 +6,15 @@ import (
 )
 
 func RegisterRoutes(router *mux.Router){
-
 	router.HandleFunc("/users", handlers.CreateUser).Methods("POST")
 	router.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
 	router.HandleFunc("/users/{id}", handlers.UpdateUser).Methods("PUT")
 	router.HandleFunc("/users/{id}", handlers.DeleteUser).Methods("DELETE")
-
+}
+func AuthRoutes(router *mux.Router){
+	router.HandleFunc("/register", )
+	router.HandleFunc("/login", )
+	router.HandleFunc("/logout", )
+	router.HandleFunc("/auth/{provider}/callback", handlers.CallbackHandler)
+	router.HandleFunc("/auth/{provider}", handlers.AuthHandlers)
 }
