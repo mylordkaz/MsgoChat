dad9250b95fb33aaf645758e31d82c1c63ec3946
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-27 00:26:52 +0900

    rename route for user CRUD

--- services/user-service/internal/routes/routes.go
+++ services/user-service/internal/routes/routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/handlers"
 )
 
-func RegisterRoutes(router *mux.Router){
+func UserRoutes(router *mux.Router){
 	router.HandleFunc("/users", handlers.CreateUser).Methods("POST")
 	router.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
 	router.HandleFunc("/users/{id}", handlers.UpdateUser).Methods("PUT")
