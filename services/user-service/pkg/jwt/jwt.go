da316cd14184271a1a1766a786e1cf3918ad8401
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-28 14:28:44 +0900

    feat: implement generation token func

--- services/user-service/pkg/jwt/jwt.go
+++ services/user-service/pkg/jwt/jwt.go
@@ -1,2 +1,34 @@
 package jwt
 
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("your_secret_here")
+
+type Claims struct {
+	UserID 		int64	`json:"user_id"`
+	Email 		string	`json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID int64, email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	claims := &Claims{
+		UserID: userID,
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
