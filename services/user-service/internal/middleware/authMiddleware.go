3d1fe1dc9e74775126d146a5dbbd07cf34b1473a
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-01 18:20:48 +0900

    add context to store user email and pass to dashB

--- services/user-service/internal/middleware/authMiddleware.go
+++ services/user-service/internal/middleware/authMiddleware.go
@@ -1,10 +1,13 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
-
 	"github.com/mylordkaz/MsgoChat/services/user-service/pkg/jwt"
 )
+type ContextKey string
+
+const UserContextKey ContextKey = "userEmail"
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
@@ -13,11 +16,14 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		_, err = jwt.VerifyJWT(cookie.Value)
+		claims, err := jwt.VerifyJWT(cookie.Value)
 		if err != nil {
 			http.Error(w, "invalid token", http.StatusUnauthorized)
 			return
 		}
-		next.ServeHTTP(w, r)
+
+		// store user email in context
+		ctx := context.WithValue(r.Context(), UserContextKey, claims.Email)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
