5378ca645c1e99e2c171e9368ad1a9c874080218
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-06 22:03:32 +0900

    refactor the test for the main package

--- services/user-service/test/main_test.go
+++ services/user-service/test/main_test.go
@@ -1,41 +1,47 @@
-package main_test
+package test
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
 	"github.com/gorilla/mux"
+	"github.com/mylordkaz/MsgoChat/services/user-service/internal/auth"
+	"github.com/mylordkaz/MsgoChat/services/user-service/internal/handlers"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/routes"
-	"github.com/mylordkaz/MsgoChat/services/user-service/cmd"
-	
 )
 
+func TestRouterSetup(t *testing.T) {
+    router := mux.NewRouter()
+    
+    routes.UserRoutes(router)
+    routes.AuthRoutes(router)
+    auth.NewAuth(router)
+    router.HandleFunc("/", handlers.HandleUser)
 
-func TestHandlerUser(t *testing.T){
-	router := mux.NewRouter()
-	routes.UserRoutes(router)
-	//router.HandleFunc("/", HandleUser)
+    // test cases
+    testCases := []struct {
+        name            string
+        path            string
+        method          string
+        expectedStatus  int
+    }{
+        {"Root Path", "/", "GET", http.StatusOK},
+    }
 
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil{
-		t.Fatal(err)
-	}
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T){
+            req, err := http.NewRequest(tc.method, tc.path, nil)
+            if err != nil {
+                t.Fatal(err)
+            }
 
-	// create a ResponseRecorder to record the response 
-	rr := httptest.NewRecorder()
-	handler := http.Handler(router)
+            rr := httptest.NewRecorder()
+            router.ServeHTTP(rr, req)
 
-	// dispach request to handler
-	handler.ServeHTTP(rr, req)
-
-	//check status code is what expected
-	if status := rr.Code; status != http.StatusOK{
-		t.Errorf("handler returned wrong status code : got %v want %v", status, http.StatusOK)
-	}
-
-	// check response body
-	expected := "Hello, you've reached the user service\n"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body : got %v want %v", rr.Body.String(), expected)
-	}
+            if status := rr.Code; status != tc.expectedStatus {
+                t.Errorf("handler returned wrong status code: got %v want %v", status, tc.expectedStatus)
+            }
+        })
+    }
 }
