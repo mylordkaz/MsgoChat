f11d5f927699c4041ddab9c4438621cbab9eb4ff
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-08 23:27:02 +0900

    feat: func to load the config

--- services/auth-service/cmd/main.go
+++ services/auth-service/cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/config"
+)
+
 func main(){
 	// Load config
-
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+	
 	// Initialsize database
 
 	// Initialize router
