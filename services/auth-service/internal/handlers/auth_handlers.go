66afe8e2be48ee8ec1742166524b090bd636ae40
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-09 23:37:06 +0900

    feat: implement register function

--- services/auth-service/internal/handlers/auth_handlers.go
+++ services/auth-service/internal/handlers/auth_handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/models"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/pkg/database"
+	"github.com/mylordkaz/MsgoChat/services/auth-service/pkg/hash"
 )
 
 type AuthHandler struct {
@@ -15,6 +18,25 @@ func NewAuthHandler(db *database.Database) *AuthHandler {
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request){
-	var user models.user
-	
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return 
+	}
+
+	hashedPassword, err := hash.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	// save user to db
+	if err := h.db.createUser(&user); err != nil {
+		http.Error(w, "Error creating user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
 }
