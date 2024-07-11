2850bdb84a9b9e4f76706bbb1ba93348c6be830c
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-11 22:43:13 +0900

    feat: generate token function

--- services/auth-service/pkg/utils/jwt/jwt.go
+++ services/auth-service/pkg/utils/jwt/jwt.go
@@ -1,10 +1,27 @@
 package jwt
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 var secretKey = []byte("secret-key") // in prod: NO HARDCODED
 
-type claims struct {
+type Claims struct {
 	UserID int `json:"user_id"`
 	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID int, duration time.Duration) (string, error) {
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
 }
