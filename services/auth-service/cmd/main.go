dc8cf85d9cc8ba1035670732be11c813c0020cea
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-12 23:30:12 +0900

    add routes , handler and start server

--- services/auth-service/cmd/main.go
+++ services/auth-service/cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/config"
+	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/handlers"
+	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/repository"
+	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/service"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/pkg/database"
 )
 
@@ -20,12 +25,25 @@ func main(){
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
 	defer db.Close()
-	
-	// Initialize router
+
+	// Initialize repository
+	authRepo := repository.NewAuthRepository(db.DB)
+
+	// Initialize service
+	authService := service.NewAuthService(authRepo)
 
 	// Initialize handlers
+	authHandler := handlers.NewAuthHandler(authService)
+
+	// Initialize router
+	r := mux.NewRouter()
 
 	// Setup routes
+	r.HandleFunc("/register", authHandler.Register).Methods("POST")
+	r.HandleFunc("/login", authHandler.Login).Methods("POST")
 
 	// start server
+	log.Printf("Starting server on %s", cfg.ServerAdress)
+    log.Fatal(http.ListenAndServe(cfg.ServerAdress, r))
+
 }
