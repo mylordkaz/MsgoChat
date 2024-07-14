634a10ff341d7bff93e0d74a7b12cdd085824104
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-14 20:03:07 +0900

    feat: add the user ID to request context

--- services/auth-service/internal/middleware/auth_middleware.go
+++ services/auth-service/internal/middleware/auth_middleware.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 	"strings"
+
+	"github.com/mylordkaz/MsgoChat/services/auth-service/pkg/utils/jwt"
 )
 
 
@@ -20,6 +23,15 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
+		claims, err := jwt.VerifyToken(bearerToken[1])
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		// add user ID to the request context
+		ctx := context.WithValue(r.Context(), "userID", claims.UserID)
+		next.ServeHTTP(w, r.WithContext(ctx))
 		
 	}
 }
