f71e0fc2d38d865b0811adb12bb8c2c333568a86
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-17 23:56:12 +0900

    modify the models

--- services/auth-service/internal/models/auth_models.go
+++ services/auth-service/internal/models/auth_models.go
@@ -3,8 +3,11 @@ package models
 type User struct {
 	ID			int		`json:"id"`
 	Username	string	`json:"username"`
-	Password	string	`json:"-"`
 	Email		string	`json:"email"`
+	Password	string	`json:"-"`
+	GoogleID 	string	`json:"-"`
+	GithubID 	string	`json:"-"`
+	Provider 	string	`json:"provider"` // local, google, github, etc.
 }
 
 type LoginRequest struct {
