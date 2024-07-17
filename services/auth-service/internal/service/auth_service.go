61fc30675ef356dcb8e5497ec4c515a7a774663c
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-17 23:56:47 +0900

    create new function to login/register Oauth user

--- services/auth-service/internal/service/auth_service.go
+++ services/auth-service/internal/service/auth_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
+	"github.com/markbates/goth"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/models"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/internal/repository"
 	"github.com/mylordkaz/MsgoChat/services/auth-service/pkg/utils/hash"
@@ -32,6 +34,11 @@ func (s *AuthService) LoginUser(username, password string) (string, error) {
     if err != nil {
         return "", err
     }
+	
+	if user.Provider != "local" {
+        return "", errors.New("this account uses OAuth, please login with the appropriate provider")
+    }
+
 
     if !hash.CheckPasswordHash(password, user.Password) {
         return "", errors.New("invalid credentials")
@@ -40,4 +47,34 @@ func (s *AuthService) LoginUser(username, password string) (string, error) {
     return jwt.GenerateToken(user.ID, 24*time.Hour)
 }
 
-// add other service e.g : getuserbyid
+func (s *AuthService) LoginOrRegisterOAuthUser(gothUser goth.User) (string, error) {
+	var user *models.User
+	var err error
+	user, err = s.repo.GetUserByProviderId(gothUser.Provider, gothUser.UserID)
+	if err != nil {
+		user, err = s.repo.GetUserByEmail(gothUser.Email)
+		if err != nil {
+			newUser := &models.User{
+				Username: gothUser.NickName,
+				Email: gothUser.Email,
+				Provider: gothUser.Provider,
+			}
+
+			switch gothUser.Provider {
+			case "google":
+				newUser.GoogleID = gothUser.UserID
+			case "github":
+				newUser.GithubID = gothUser.UserID
+			}
+
+			err = s.repo.CreateUser(newUser)
+			if err != nil {
+				return "", err
+			}
+			user = newUser
+		} else {
+			return "", fmt.Errorf("email already in use with another account")
+		}
+	}
+	return jwt.GenerateToken(user.ID, 24*time.Hour)
+}
