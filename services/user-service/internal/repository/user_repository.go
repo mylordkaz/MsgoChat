700264c4b15c0ca7ae149d7758e82fad88fe1a1b
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-19 22:41:53 +0900

    initialize repo

---
+++ services/user-service/internal/repository/user_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
+)
+
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) CreateUser(user *models.User) (*models.User, error) {
+	// implement db insertion logic
+}
+func (r *UserRepository) GetUser(id string) (*models.User, error) {
+	// implement db retrival logic
+}
+func (r *UserRepository) UpdateUser(user *models.User) (*models.User, error) {
+	// implement db update logic
+}
+func (r *UserRepository) DeleteUser(id string) (*models.User, error) {
+	// implement db deletion logic
+}
