3325e83e8ba213ad915e0c69e6bf1a26113571aa
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-19 22:42:33 +0900

    initialize db

---
+++ services/user-service/pkg/database/database.go
@@ -0,0 +1,16 @@
+package database
+
+import "database/sql"
+
+func Connect(databaseURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
