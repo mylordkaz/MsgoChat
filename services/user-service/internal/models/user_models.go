c0314cb4def38b65788722240fd2afc9a6d429a2
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-20 16:19:41 +0900

    update user model

--- services/user-service/internal/models/user_models.go
+++ services/user-service/internal/models/user_models.go
@@ -6,6 +6,7 @@ type User struct {
 	ID			string 		`json:"id"`
 	Username	string		`json:"username"`
 	Email 		string		`json:"email"`
+	AvatarURl	string		`json:"avatar_url"`
 	CreatedAt	time.Time	`json:"created_at"`
 	UpdatedAt	time.Time	`json:"updated_at"`
 }
