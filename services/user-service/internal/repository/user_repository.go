2fd35f0096bdbd03167f77ff39cf6a9965a8b030
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-21 16:12:53 +0900

    basic CRUD query func

--- services/user-service/internal/repository/user_repository.go
+++ services/user-service/internal/repository/user_repository.go
@@ -16,14 +16,60 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) CreateUser(user *models.User) (*models.User, error) {
-	// implement db insertion logic
+	query := `
+		INSERT INTO users (id, username, email, avatar_url, created_at, updated_at)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id, username, email, avatar_url, created_at, updated_at
+	`
+
+	err := r.db.QueryRow(query,
+		user.ID, user.Username, user.Email, user.AvatarURl, user.CreatedAt, user.UpdatedAt,
+	).Scan(
+		&user.ID, &user.Username, &user.Email, &user.AvatarURl, &user.CreatedAt, &user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
+
 func (r *UserRepository) GetUser(id string) (*models.User, error) {
-	// implement db retrival logic
+	query := `
+		SELECT id, username, email, avatarURL, created_at, updated_at
+		FROM users WHERE id = $1
+	`
+
+	var user models.User
+	err :=r.db.QueryRow(query, id).Scan(
+		&user.ID, &user.Username, &user.Email, &user.AvatarURl, &user.CreatedAt, &user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
+
 func (r *UserRepository) UpdateUser(user *models.User)  error {
-	// implement db update logic
+	query := `
+		UPDATE users
+		SET username = $2, email = $3, avatar_url $4, updated_at = $5
+		WHERE id = $1
+	`
+	_, err := r.db.Exec(query,
+		user.ID, user.Username, user.Email, user.AvatarURl, user.UpdatedAt,
+	)
+	return err
 }
+
 func (r *UserRepository) DeleteUser(id string) error {
-	// implement db deletion logic
+	query := `
+		DELETE FROM users WHERE id = $1
+	`
+	_, err := r.db.Exec(query, id)
+
+	return err
 }
