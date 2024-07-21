6698343d52039e79b8522d719b44ac83bfc82143
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-21 16:19:25 +0900

    func to get users list

--- services/user-service/internal/repository/user_repository.go
+++ services/user-service/internal/repository/user_repository.go
@@ -73,3 +73,31 @@ func (r *UserRepository) DeleteUser(id string) error {
 
 	return err
 }
+
+func (r *UserRepository) ListUsers(limit, offset int) ([]*models.User, error) {
+	query := `
+		SELECT id ,username, email, avatar_url, created_at, updated_at
+		FROM users
+		ORDER BY  created_at DESC
+		LIMIT $1 OFFSET $2
+	`
+
+	rows, err := r.db.Query(query, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []*models.User
+	for rows.Next() {
+		var user models.User
+		err := rows.Scan(
+			&user.ID, &user.Username, &user.Email, &user.AvatarURl, &user.CreatedAt, &user.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users,  &user)
+	}
+	return users, nil
+}
