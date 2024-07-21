c21361f132d03515dfd4e88a3648823d67e6a25e
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-21 16:52:21 +0900

    create a func to validate user data

--- services/user-service/pkg/utils/utils.go
+++ services/user-service/pkg/utils/utils.go
@@ -3,6 +3,11 @@ package utils
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
+	"regexp"
+	"strings"
+
+	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
 )
 
 func GenerateRandomID() (string, error) {
@@ -12,4 +17,28 @@ func GenerateRandomID() (string, error) {
 		return "", err
 	}
 	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func ValidateUser(user *models.User) error {
+	// check username
+	if strings.TrimSpace(user.Username) == "" {
+		return errors.New("username cannot be empty")
+	}
+	if len(user.Username) < 3 || len(user.Username) > 30 {
+		return errors.New("username must be between 3 to 30 characters")
+	}
+	if !regexp.MustCompile(`^[a-zA-Z0-9_]+$`).MatchString(user.Username) {
+		return errors.New("username can only contain alphanumeric characters and underscore")
+	}
+
+	// check email
+	if strings.TrimSpace(user.Email) == "" {
+		return errors.New("email cannot be empty")
+	}
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	if !emailRegex.MatchString(user.Email) {
+		return errors.New("invalid email format")
+	}
+
+	return nil
 }
