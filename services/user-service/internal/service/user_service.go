ea9052642fef464572ed446c8e74bc5270d206ba
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-21 17:21:30 +0900

    write service logic for user

--- services/user-service/internal/service/user_service.go
+++ services/user-service/internal/service/user_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"time"
+
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/models"
 	"github.com/mylordkaz/MsgoChat/services/user-service/internal/repository"
+	"github.com/mylordkaz/MsgoChat/services/user-service/pkg/utils"
 )
 
 
@@ -15,6 +18,22 @@ func NewUserService(userRepo *repository.UserRepository) *UserService {
 }
 
 func (s *UserService) CreateUser(user *models.User) (*models.User, error) {
+	// generate new ID
+	id, err := utils.GenerateRandomID()
+	if err != nil {
+		return nil, err
+	}
+	user.ID = id
+
+	// timestamp
+	now := time.Now()
+	user.CreatedAt = now
+	user.UpdatedAt = now
+
+	// validate data
+	if err := utils.ValidateUser(user); err != nil {
+		return nil, err
+	}
 	return s.userRepo.CreateUser(user)
 }
 
@@ -23,9 +42,26 @@ func (s *UserService) GetUser(id string) (*models.User, error) {
 }
 
 func (s *UserService) UpdateUser(user *models.User) error {
+
+	if err := utils.ValidateUser(user); err != nil {
+		return err
+	}
+
+	user.UpdatedAt = time.Now()
 	return s.userRepo.UpdateUser(user)
 }
 
 func (s *UserService) DeleteUser(id string) error {
 	return s.userRepo.DeleteUser(id)
+}
+
+func (s *UserService) ListUsers(page, pageSize int) ([]*models.User, error) {
+	if page < 1 {
+		page = 1
+	}
+	if pageSize < 1 {
+		pageSize = 10
+	}
+	offset := (page - 1) * pageSize
+	return s.userRepo.ListUsers(pageSize, offset)
 }
